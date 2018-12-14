b6454e026b58603a86ab9ba409fd95c78b41aac2
radionov <boss@flagstudio.ru>
2018-12-14 11:13:05 +0500

    composer-link in Laravel aliases

--- laravel_aliases.sh
+++ laravel_aliases.sh
@@ -5,3 +5,8 @@
 alias pa='php artisan'
 alias pu='./vendor/bin/phpunit --testdox'
 alias mfs='php artisan migrate:fresh --seed'
+
+# http://calebporzio.com/bash-alias-composer-link-use-local-folders-as-composer-dependancies/
+composer-link() {
+    composer config repositories.local '{"type": "path", "url": "'$1'"}' --file composer.json
+}
