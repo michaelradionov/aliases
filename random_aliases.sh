bfdb7b3a63c73f66b91e1c7349ef0952a3a54a47
Michael Radionov <14803637+michaelradionov@users.noreply.github.com>
2019-08-12 20:28:02 +0500

    Update random_aliases.sh

--- random_aliases.sh
+++ random_aliases.sh
@@ -2,7 +2,9 @@
 # Random aliases
 # https://github.com/michaelradionov/aliases
 
-alias la="ls -alh"
-alias b="m ~/.bashrc"
-alias hosts="cat ~/.ssh/config"
-alias dudu="du --max-depth=1 -h | sort -h"
+alias la="ls -alh" Better ls
+alias b="m ~/.bashrc"  Edit ~/.bashrc file
+alias hosts="cat ~/.ssh/config" # List all your ssh hosts shortcuts
+alias dudu="du --max-depth=1 -h | sort -h" # check current folder disk space
+alias passgen='openssl rand -base64 6' # Generate strong password
+alias genpass='passgen' # I just can't remember previous alias :)
