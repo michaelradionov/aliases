f51c8c8ba381bd722426ac7d154b248b558d5351
Michael Radionov <14803637+michaelradionov@users.noreply.github.com>
2019-09-04 13:18:16 +0500

    phpinfo alias

--- random_aliases.sh
+++ random_aliases.sh
@@ -6,5 +6,8 @@ alias la="ls -alh" Better ls
 alias b="m ~/.bashrc"  Edit ~/.bashrc file
 alias hosts="cat ~/.ssh/config" # List all your ssh hosts shortcuts
 alias dudu="du --max-depth=1 -h | sort -h" # check current folder disk space
+
 alias passgen='openssl rand -base64 6' # Generate strong password
 alias genpass='passgen' # I just can't remember previous alias :)
+
+alias phpinfo='echo "<?php phpinfo(); ?>" > info.php'
