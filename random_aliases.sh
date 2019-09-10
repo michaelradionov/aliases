eac7771a002c3673680bcb7b3e4d0f978187531b
Michael Radionov <14803637+michaelradionov@users.noreply.github.com>
2019-09-10 14:08:05 +0500

    Update random_aliases.sh

--- random_aliases.sh
+++ random_aliases.sh
@@ -2,8 +2,8 @@
 # Random aliases
 # https://github.com/michaelradionov/aliases
 
-alias la="ls -alh" Better ls
-alias b="m ~/.bashrc"  Edit ~/.bashrc file
+alias la="ls -alh" # Better ls
+alias b="m ~/.bashrc" # Edit ~/.bashrc file
 alias hosts="cat ~/.ssh/config" # List all your ssh hosts shortcuts
 alias dudu="du --max-depth=1 -h | sort -h" # check current folder disk space
 
